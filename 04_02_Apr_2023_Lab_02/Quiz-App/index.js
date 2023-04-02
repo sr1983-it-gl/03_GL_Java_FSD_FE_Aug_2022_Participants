997fa0573494074f58e61a3ece49d235cb62bdc0
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-04-02 15:21:24 +0530

    Added support for Questions

---
+++ 04_02_Apr_2023_Lab_02/Quiz-App/index.js
@@ -0,0 +1,11 @@
+function Question (questionText, questionNo){
+
+  this.questionText = questionText;
+  this.questionNo = questionNo; 
+}
+
+const q1 = new Question("JavaScript supports", 1);
+const q2 = new Question("Which language is used for styling web pages?", 2);
+const q3 = new Question("Which is not a JavaScript Framework?", 3);
+const q4 = new Question("Which is used for Connect To Database?", 4);
+const q5 = new Question("JavaScript is a", 5);
